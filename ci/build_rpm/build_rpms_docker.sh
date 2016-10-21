e6ccc6e5888c6026646b5d3cb21bb3a4272b0d27
Zhijiang <hu.zhijiang@zte.com.cn>
2016-10-21 01:21:30 -0400

    Automatically remove the container when it exits
    Change-Id: I7ce146ef4dc8717e782600750660dddf45e3047f
    Signed-off-by: Zhijiang <hu.zhijiang@zte.com.cn>

--- ci/build_rpm/build_rpms_docker.sh
+++ ci/build_rpm/build_rpms_docker.sh
@@ -8,17 +8,5 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-tmp_rpm_build_dir=/root/daisy4nfv
-rpm_build_dir=/opt/daisy4nfv
-tmp_rpm_output_dir=$tmp_rpm_build_dir/build_output
-rpm_output_dir=$rpm_build_dir/build_output
-cp -r $rpm_build_dir $tmp_rpm_build_dir
 
-# Build daisy rpm packages
-cd $tmp_rpm_build_dir/
-make clean
-./ci/build_rpm/daisy_rpm_build.sh build_output
-
-
-# Move daisy bin file from tmp_output_dir to output_dir
-mv $tmp_rpm_output_dir/installdaisy_el7_noarch.bin $rpm_output_dir
+# Implement build (call make allrpm) here
