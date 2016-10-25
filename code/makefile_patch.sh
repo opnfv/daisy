163f3d1509786e2cbff62e61bbd7a1ba20e46914
Yao Lu <lu.yao135@zte.com.cn>
2016-10-25 09:04:16 +0800

    add patch of openstack/daisycloud
    the patch is about integrate kolla tar and tgz and centos to daisy bin 
    and cp to appoint path
    
    Change-Id: I3043d2a38f15cdff44f9c777bff8bb5e517684bd
    Signed-off-by: Yao Lu <lu.yao135@zte.com.cn>

---
+++ code/makefile_patch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016 ZTE Corporation and others.
+# hu.zhijiang@zte.com.cn
+# lu.yao135@zte.com.cn
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+TOOLS_PATH=$1
+CACHE_PATH=/home/cache
+if [ ! -d $CACHE_PATH ]; then mkdir -p $CACHE_PATH ; fi
+if [ ! -f $CACHE_PATH/CentOS-7-x86_64-Minimal-1511.iso ]; then
+    wget -P $CACHE_PATH "http://mirrors.163.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1511.iso"
+fi
+if [ ! -f $CACHE_PATH/registry-2.0.3.tgz ]; then
+    wget -P $CACHE_PATH "ftp://openuser:123@120.76.145.166/registry-2.0.3.tgz"
+fi
+if [ ! -f $CACHE_PATH/registry-server.tar ]; then
+    wget -P $CACHE_PATH "ftp://openuser:123@120.76.145.166/registry-server.tar"
+fi
+cp $CACHE_PATH/CentOS-7-x86_64-Minimal-1511.iso $TOOLS_PATH/setup/bin_temp/
+cp $CACHE_PATH/registry-2.0.3.tgz $TOOLS_PATH/setup/bin_temp/
+cp $CACHE_PATH/registry-server.tar $TOOLS_PATH/setup/bin_temp/
+cp $TOOLS_PATH/setup/install_interface_patch.sh $TOOLS_PATH/setup/bin_temp/
+chmod +x $TOOLS_PATH/setup/bin_temp/install_interface_patch.sh
