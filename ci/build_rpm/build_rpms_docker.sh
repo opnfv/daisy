74aab5f31fe90ff73b63ac6ea460b995cd7e1e1a
Yao Lu <lu.yao135@zte.com.cn>
2016-10-25 09:04:16 +0800

    add patch of openstack/daisycloud
    the patch is about integrate kolla tar and tgz and centos to daisy bin
    and cp to appoint path
    
    Change-Id: I3043d2a38f15cdff44f9c777bff8bb5e517684bd
    Signed-off-by: Yao Lu <lu.yao135@zte.com.cn>

--- ci/build_rpm/build_rpms_docker.sh
+++ ci/build_rpm/build_rpms_docker.sh
@@ -9,4 +9,44 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-# Implement build (call make allrpm) here
+rpm_build_dir=/opt/daisy4nfv
+rpm_output_dir=$rpm_build_dir/build_output
+tmp_rpm_build_dir=/root/daisy4nfv
+tmp_rpm_output_dir=$tmp_rpm_build_dir/build_output
+
+cp -r $rpm_build_dir $tmp_rpm_build_dir
+cd $tmp_rpm_build_dir
+
+echo "#########################################################"
+echo "               systemctl info:                   "
+echo "#########################################################"
+echo "###Uname: $(uname)"
+echo "###Hostname: $(hostname)"
+
+maxcount=3
+cnt=0
+rc=1
+while [ $cnt -lt $maxcount ] && [ $rc -ne 0 ]
+do
+    cnt=$[cnt + 1]
+    echo -e "\n\n\n*** Starting build attempt # $cnt"
+
+    git clone https://git.openstack.org/openstack/daisycloud-core
+    cp $tmp_rpm_build_dir/code/makefile_patch.sh daisycloud-core/tools/setup
+    cp $tmp_rpm_build_dir/code/install_interface_patch.sh daisycloud-core/tools/setup
+    cd daisycloud-core/make
+    make allrpm
+    rc=$?
+
+    echo "######################################################"
+    echo "          done              "
+    echo "######################################################"
+    if [ $rc -ne 0 ]; then
+        echo "### Build failed with rc $rc ###"
+    else
+        echo "### Build successfully at attempt # $cnt"
+    fi
+done
+cd ..
+cp target/el7/noarch/installdaisy_el7_noarch.bin $rpm_output_dir
+exit $rc
