37c4cc04d0a3ac8891888f5625efb3b40ad0ac96
Yao Lu <lu.yao135@zte.com.cn>
2016-11-09 18:40:48 +0800

    modify iso and tar path
    Change-Id: I526c814a7895bdf2ed97af5cb51e6140a542c7fd
    Signed-off-by: Yao Lu <lu.yao135@zte.com.cn>

--- code/makefile_patch.sh
+++ code/makefile_patch.sh
@@ -12,16 +12,16 @@ TOOLS_PATH=$1
 CACHE_PATH=/home/cache
 if [ ! -d $CACHE_PATH ]; then mkdir -p $CACHE_PATH ; fi
 if [ ! -f $CACHE_PATH/CentOS-7-x86_64-Minimal-1511.iso ]; then
-    wget -P $CACHE_PATH "http://mirrors.163.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1511.iso"
+    wget -P $CACHE_PATH "http://ftp.osuosl.org/pub/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1511.iso"
 fi
-if [ ! -f $CACHE_PATH/registry-2.0.3.tgz ]; then
-    wget -P $CACHE_PATH "ftp://openuser:123@120.76.145.166/registry-mitaka-latest.tgz"
+if [ ! -f $CACHE_PATH/registry-mitaka-latest.tgz ]; then
+    wget -P $CACHE_PATH "http://daisycloud.org/static/files/registry-mitaka-latest.tgz"
 fi
 if [ ! -f $CACHE_PATH/registry-server.tar ]; then
-    wget -P $CACHE_PATH "ftp://openuser:123@120.76.145.166/registry-server.tar"
+    wget -P $CACHE_PATH "wget http://daisycloud.org/static/files/registry-server.tar"
 fi
 cp $CACHE_PATH/CentOS-7-x86_64-Minimal-1511.iso $TOOLS_PATH/setup/bin_temp/
-cp $CACHE_PATH/registry-2.0.3.tgz $TOOLS_PATH/setup/bin_temp/
+cp $CACHE_PATH/registry-mitaka-latest.tgz $TOOLS_PATH/setup/bin_temp/
 cp $CACHE_PATH/registry-server.tar $TOOLS_PATH/setup/bin_temp/
 cp $TOOLS_PATH/setup/install_interface_patch.sh $TOOLS_PATH/setup/bin_temp/
 chmod +x $TOOLS_PATH/setup/bin_temp/install_interface_patch.sh
