34250bd0a06aa9795a70f75969b3f024e5bc9db9
Yao Lu <lu.yao135@zte.com.cn>
2016-11-29 22:39:37 +0800

    implement deploy on virthal pod
    Change-Id: I600e21f0d36f37c75cac4ace5f7225e32ab93d56
    Signed-off-by: Yao Lu <lu.yao135@zte.com.cn>

---
+++ deploy/check_os_progress.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source /root/daisyrc_admin
+cluster_id=`daisy cluster-list | awk -F "|" '{print $2}' | sed -n '4p'`
+daisy install $cluster_id --skip-pxe-ipmi true
+echo "check os installing progress..."
+while true; do
+    os_install_active=`daisy host-list --cluster-id $cluster_id | awk -F "|" '{print $8}' | grep -c "active" `
+    os_install_failed=`daisy host-list --cluster-id $cluster_id | awk -F "|" '{print $8}' | grep -c "install-failed" `
+    if [ $os_install_active -eq 1 ]; then
+        echo "os installing successful ..."
+        break
+    elif [ $os_install_failed -gt 0 ]; then
+        echo "os installing have failed..."
+        exit 1
+    else
+        progress=`daisy host-list --cluster-id $cluster_id |grep DISCOVERY_SUCCESSFUL |awk -F "|" '{print $7}'|sed s/[[:space:]]//g`
+        echo "os in installing, the progress is $progress%"
+        sleep 10
+    fi
+done
+systemctl disable dhcpd
+systemctl stop dhcpd
