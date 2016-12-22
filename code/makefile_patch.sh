c7dae15fa328737dd3b80f0c823bb2f2efdc085a
Alex Yang <yangyang1@zte.com.cn>
2016-12-23 00:25:07 +0800

    Update the url to download centos iso
    The url http://ftp.osuosl.org/pub/centos/7/isos/x86_64/
    CentOS-7-x86_64-Minimal-1511.iso is invalid now.
    Only 1611 version can be downloaded from that website.
    So the url should be modified.
    
    Change-Id: I1208ddabf940fb711d871fb884b8b2a1d7213c27
    Signed-off-by: Alex Yang <yangyang1@zte.com.cn>

--- code/makefile_patch.sh
+++ code/makefile_patch.sh
@@ -14,9 +14,11 @@ imagebranch="newton"
 imageversion="latest"
 imageserver="http://120.24.17.215"
 imagename="kolla-image-$imagebranch-$imageversion.tgz"
+isoname="CentOS-7-x86_64-Minimal-1511.iso"
+isourl="http://buildlogs.centos.org/rolling/7/isos/x86_64/${isoname}"
 if [ ! -d $CACHE_PATH ]; then mkdir -p $CACHE_PATH ; fi
-if [ ! -f $CACHE_PATH/CentOS-7-x86_64-Minimal-1511.iso ]; then
-    wget -P $CACHE_PATH "http://ftp.osuosl.org/pub/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1511.iso"
+if [ ! -f $CACHE_PATH/${isoname} ]; then
+    wget -P $CACHE_PATH $isourl
 fi
 if [ ! -f $CACHE_PATH/$imagename ]; then
     wget -P $CACHE_PATH "$imageserver/$imagename"
@@ -24,7 +26,7 @@ fi
 if [ ! -f $CACHE_PATH/registry-server.tar ]; then
     wget -P $CACHE_PATH "http://daisycloud.org/static/files/registry-server.tar"
 fi
-cp $CACHE_PATH/CentOS-7-x86_64-Minimal-1511.iso $TOOLS_PATH/setup/bin_temp/
+cp $CACHE_PATH/${isoname} $TOOLS_PATH/setup/bin_temp/
 cp $CACHE_PATH/$imagename $TOOLS_PATH/setup/bin_temp/
 cp $CACHE_PATH/registry-server.tar $TOOLS_PATH/setup/bin_temp/
 cp $TOOLS_PATH/setup/install_interface_patch.sh $TOOLS_PATH/setup/bin_temp/
