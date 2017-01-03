0184c6dc5cd506179051aeba15e95cf47b9d8ea8
Julien <zhang.jun3g@zte.com.cn>
2017-01-04 00:42:01 +0800

    Run docker from dockerhub
    1. Use the build image in releng;
    2. Pull down the newest image and run;
    3. Delete unused codes;
    
    wait from docker image build(26449) and upload in the dockerhub
    
    Change-Id: I72b072aff6cd905b426451768dc51d17eee6c0a3
    Signed-off-by: Julien <zhang.jun3g@zte.com.cn>

--- ci/build_rpm/build_rpms.sh
+++ ci/build_rpm/build_rpms.sh
@@ -11,51 +11,22 @@
 set -eux
 DAISYDIR=$1
 OPNFV_ARTIFACT_VERSION=$2
-
 function build_rpm_pkg {
     # Cleanup prev build resutls
     rm -rf $DAISYDIR/build_output
     mkdir -p $DAISYDIR/build_output
 
-    sudo docker build -t daisy4nfv_rpm ../../docker
-    sudo docker run --rm -v $DAISYDIR:/opt/daisy4nfv \
+    sudo docker run --rm -t \
+                    -v $DAISYDIR:/opt/daisy4nfv \
                     -v $CACHE_DIRECTORY:/home/cache \
-                    -t daisy4nfv_rpm \
                     /opt/daisy4nfv/ci/build_rpm/build_rpms_docker.sh \
-                    $OPNFV_ARTIFACT_VERSION
-}
-
-function cleanup_container {
-    containers_to_kill=$(sudo docker ps -a \
-                                        --filter "label=daisy_image_version" \
-                                        --format "{{.Names}}")
-
-    if [[ ! -z "$containers_to_kill" ]]; then
-        ops='{{range .Mounts}} {{printf "%s\n" .Name }}{{end}}'
-        volumes_to_remove=$(sudo docker inspect -f \
-                                                "$ops" ${containers_to_kill} \
-                                                | egrep -v '(^\s*$)' \
-                                                | sort | uniq)
-
-        sudo docker stop -t 2 ${containers_to_kill} >/dev/null 2>&1
-        sudo docker rm -v -f ${containers_to_kill} >/dev/null 2>&1
-
-        if [[ ! -z "$volumes_to_remove" ]]; then
-            sudo docker volume rm ${volumes_to_remove} >/dev/null 2>&1
-        fi
-    fi
+                    --name daisy \
+                    opnfv/daisy $OPNFV_ARTIFACT_VERSION
 }
 
 function cleanup_docker_image {
-    images_to_delete=$(sudo docker images -q \
-                                          --filter "label=daisy_image_version")
-
-    echo "Removing images... $images_to_delete"
-    if [[ ! -z "$images_to_delete" ]]; then
-        sudo docker rmi -f ${images_to_delete} >/dev/null 2>&1
-    fi
+    [[ ! -z $(sudo docker images -q opnfv/daisy) ]] && sudo docker rmi opnfv/daisy
 }
 
-cleanup_container
 cleanup_docker_image
 build_rpm_pkg
