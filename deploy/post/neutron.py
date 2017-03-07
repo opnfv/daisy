63449c1c4ec9c09d65fcbcf17d33b799abc746fa
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-03-07 14:36:03 +0800

    create admin external subnet
    Change-Id: Ib56c94c090fd2fc0950ce97e1c4c1fdf622f2d08
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- deploy/post/neutron.py
+++ deploy/post/neutron.py
@@ -16,18 +16,40 @@ class Neutron(object):
         session = keystoneauth.Keystoneauth(openrc).session
         self.client = neutronclient.Client(api_v, session=session)
 
+    def create_network(self, name, body):
+        if not self.is_network_exist(name):
+            return self._create_network(name, body)
+        else:
+            print('admin_ext [{}] already exist'.format(name))
+            return None
+
+    def create_subnet(self, body=None):
+        if not self.is_subnet_exist(body):
+            return self._create_subnet(body)
+        else:
+            print ('subnet [{}] already exist'.format(body))
+            return None
+        
     def list_networks(self):
         return self.client.list_networks()['networks']
 
-    def create_network(self, name, body):
-        if not self.is_network_exist(name):
-            self._create_network(name, body)
-        else:
-            print('admin_ext [{}] already exist'.format(name))
-        pass
+    def list_subnets(self):
+        return self.client.list_subnets()['subnets']
 
     def is_network_exist(self, name):
         return [] != filter(lambda n: n['name'] == name, self.list_networks())
+
+    def is_subnet_exist(self, body):
+        print 'body: {}'.format(body)
+
+        def same_subnet(n):
+            print 'n: {}'.format(n)
+            for item in ['name', 'network_id']:
+                if n[item] != body['subnets'][0][item]:
+                    return False
+            return True
+
+        return [] != filter(lambda n: same_subnet(n), self.list_subnets())
 
     def _create_network(self, name, body):
         try:
@@ -36,4 +58,14 @@ class Neutron(object):
             return nid
         except Exception, e:
             print('_create_admin_ext_net [{}] fail with: {}'.format(name, e))
+            return None
+    
+    def _create_subnet(self, body):
+        print('_create_subnet with body: {}'.format(body))
+        try:
+            snid = self.client.create_subnet(body)['subnets'][0]['id']
+            print('_create_subnet success with id={}'.format(snid))
+            return snid
+        except Exception, e:
+            print('_create_subnet fail with: {}'.format(e))
             return None
