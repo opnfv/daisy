cf307e682d5dcfea08dce32f9acf8541ca6ff1d8
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-03-14 17:53:45 +0800

    create m1.micro flavor for functest
    Change-Id: Iacb36cb05e72b0755cec0a5bd5a6c48f3ec70c6f
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

---
+++ deploy/post/nova.py
@@ -0,0 +1,24 @@
+import novaclient.client
+
+import keystoneauth
+
+
+class Nova(keystoneauth.Keystoneauth):
+    def __init__(self, version='2', openrc=None):
+        super(Nova, self).__init__(openrc)
+        self.client = novaclient.client.Client(version, session=self.session)
+        self.flavors = self.client.flavors
+
+    def create_flavor(self, name, ram, vcpus, disk, is_public=True):
+        flavor = self.flavors.create(name, ram, vcpus, disk, is_public=is_public)
+        return flavor.id
+
+    def get_flavor_by_name(self, name):
+        for flavor in self.list_flavors():
+            if flavor.name == name:
+                return flavor.id
+
+        return None
+
+    def list_flavors(self):
+        return self.flavors.list(detailed=True)
