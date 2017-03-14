efb338dfa55d7d48cbd8166e5430c1c37b1cc1c9
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-03-14 16:48:22 +0800

    create default TestVM for Functest
    Change-Id: I505a0819d5f1a4350e82ceaa9e5cbee285c8fba0
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- deploy/post/execute.py
+++ deploy/post/execute.py
@@ -6,7 +6,10 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+import os
+
 import neutron
+import glance
 
 
 def _config_admin_external_network():
@@ -44,10 +47,35 @@ def _config_admin_external_subnet(nid):
     }
 
 
+def _create_image_TestVM():
+    glanceclient = glance.Glance()
+    image = 'TestVM1'
+    if not glanceclient.get_by_name(image):
+        url = 'http://download.cirros-cloud.net'
+        version = '0.3.5'
+        name = 'cirros-{}-x86_64-disk.img'.format(version)
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), name)
+        if not os.path.isfile(path):
+            cmd = "wget %(url)s/%(version)s/%(name)s %(path)s" % {
+                'url': url,
+                'version': version,
+                'name': name,
+                'path': path}
+            os.system(cmd)
+            pass
+        try:
+            glanceclient.create(image, path)
+        except Exception as error:
+            print ('create_TestVM failed: {}'.format(str(error)))
+    else:
+        print ('Use existing TestVM image')
+
+
 def main():
     neutronclient = neutron.Neutron()
     nid = neutronclient.create_network(*(_config_admin_external_network()))
     neutronclient.create_subnet(_config_admin_external_subnet(nid))
+    _create_image_TestVM()
 
 if __name__ == '__main__':
     main()
