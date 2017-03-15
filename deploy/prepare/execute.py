fb4f5496831e2739280fb266f4ac0a70294cd8fb
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-03-15 15:50:35 +0800

    change to deferred_auth_method=password in heat-api.conf
    Change-Id: I2b61d756adbeef0efecb2a9d479e8c43c56c21fb
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- deploy/prepare/execute.py
+++ deploy/prepare/execute.py
@@ -3,19 +3,39 @@ import os
 
 from deploy.config.network import NetworkConfig
 
-NOVA_CONF_PATH = '/etc/kolla/config/nova'
-NOVA_API_CONF = '{}/nova-api.conf'.format(NOVA_CONF_PATH)
+KOLLA_CONF_PATH = '/etc/kolla/config'
+
+
+def _make_dirs(path):
+    if not os.path.isdir(path):
+        os.makedirs(path, mode=0644)
+
+
+def _write_conf_file(conf_file, conf):
+    with open(conf_file, 'w') as f:
+        f.write(conf)
+        f.close()
+
+
+def _config(service, server, conf):
+    service_conf_path = os.path.join(KOLLA_CONF_PATH, service)
+    service_server = os.path.join(service_conf_path,
+                                  '{}-{}.conf'.format(service, server))
+    _make_dirs(service_conf_path)
+    _write_conf_file(service_server, conf)
 
 
 def _config_nova_api(network_file):
     xnet = NetworkConfig(network_file=network_file).external_network
-    if not os.path.isdir(NOVA_CONF_PATH):
-        os.makedirs(NOVA_CONF_PATH, mode=0644)
+    _config('nova', 'api',
+            '[DEFAULT]\n'
+            'default_floating_pool = {}\n'.format(xnet['network_name']))
 
-    with open(NOVA_API_CONF, 'w') as f:
-        f.write('[DEFAULT]\n'
-                'default_floating_pool={}\n'.format(xnet['network_name']))
-        f.close()
+
+def _config_heat_api():
+    _config('heat', 'api',
+            '[DEFAULT]\n'
+            'deferred_auth_method = password\n')
 
 
 def main():
@@ -26,6 +46,7 @@ def main():
                         help='network configuration file')
     args = parser.parse_args()
     _config_nova_api(args.network_file)
+    _config_heat_api()
 
 
 if __name__ == '__main__':
