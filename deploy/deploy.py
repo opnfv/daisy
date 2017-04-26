ab6eb1772baba1e1109a8e01f60ba940404d45f7
Alex Yang <yangyang1@zte.com.cn>
2017-04-26 14:14:43 +0800

    Implement the deployment script with python
    1. deploy.py: control the work flow of the deployment
    2. daisy_server.py: maintain the ssh connection with daisy server
         and execute ssh commands
    3. environment.py: create/find/delete the nodes/vms
    4. libvirt_utils.py: deal with the vm templates and call virsh commands
    5. utils.py: some common functions such as file/directory/bash operation
    
    Change-Id: I1caa4b0b3118665e15410e8f02bcb6473e5a530b
    Signed-off-by: Alex Yang <yangyang1@zte.com.cn>

---
+++ deploy/deploy.py
@@ -0,0 +1,242 @@
+#!/usr/bin/python
+##############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+##############################################################################
+# TODO:
+# [ ] 1. specify VM templates (Server, Controller & Compute) in deploy.yml
+# [ ] 2. specify network templates in deploy.yml
+# [ ] 3. specify adapter(ipmi, libvirt) in deploy.yml
+# [ ] 4. get ipmi user/password from PDF (Pod Descriptor File)
+# [ ] 5. get pxe bridge from jjb
+# [ ] 6. enlarge the vm size of Controller & Compute in deploy.yml
+##############################################################################
+
+import argparse
+import os
+import time
+import yaml
+
+from utils import (
+    WORKSPACE,
+    save_log_to_file,
+    LI,
+    log_bar,
+    join,
+    check_sudo_privilege,
+    check_file_exists,
+    make_file_executable,
+    confirm_dir_exists
+)
+
+from environment import (
+    DaisyEnvironment,
+)
+
+from daisy_server import (
+    DaisyServer
+)
+
+
+class DaisyDeployment(object):
+    def __init__(self, lab_name, pod_name, deploy_file, net_file, bin_file,
+                 daisy_only, cleanup_only, remote_dir, work_dir, storage_dir,
+                 pxe_bridge, deploy_log):
+        self.lab_name = lab_name
+        self.pod_name = pod_name
+
+        self.deploy_file = deploy_file
+        with open(deploy_file) as yaml_file:
+            self.deploy_struct = yaml.load(yaml_file)
+
+        if not cleanup_only:
+            self.net_file = net_file
+            with open(net_file) as yaml_file:
+                self.net_struct = yaml.load(yaml_file)
+        else:
+            self.net_struct = None
+
+        self.bin_file = bin_file
+        self.daisy_only = daisy_only
+        self.cleanup_only = cleanup_only
+        self.remote_dir = remote_dir
+        self.work_dir = work_dir
+        self.storage_dir = storage_dir
+        self.pxe_bridge = pxe_bridge
+        self.deploy_log = deploy_log
+
+        self.adapter = self.get_adapter_info()
+        LI('The adapter is %s' % self.adapter)
+
+        # TODO: modify the jjb code to provide bridge name
+        if self.adapter == 'libvirt':
+            self.pxe_bridge = 'daisy1'
+        else:
+            self.pxe_bridge = 'br7'
+
+        self.daisy_server_info = self.get_daisy_server_info()
+
+        self.daisy_env = DaisyEnvironment(self.deploy_struct,
+                                          self.net_struct,
+                                          self.adapter,
+                                          self.pxe_bridge,
+                                          self.daisy_server_info,
+                                          self.work_dir,
+                                          self.storage_dir)
+
+    def get_adapter_info(self):
+        # TODO: specify the adapter info in deploy.yml
+        if 'adapter' in self.deploy_struct:
+            return self.deploy_struct['adapter']
+        elif self.pod_name and 'virtual' in self.pod_name:
+            return 'libvirt'
+        else:
+            return 'ipmi'
+
+    def get_daisy_server_info(self):
+        address = self.deploy_struct['daisy_ip']
+        gateway = self.deploy_struct['daisy_gateway']
+        password = self.deploy_struct['daisy_passwd']
+        disk_size = self.deploy_struct['disks']['daisy']
+        # TODO: get VM name of daisy server from deploy.yml or vm template
+        name = 'daisy'
+        image = join(self.storage_dir, name + '.qcow2')
+
+        return {'name': name,
+                'image': image,
+                'address': address,
+                'gateway': gateway,
+                'password': password,
+                'disk_size': disk_size}
+
+    def run(self):
+        self.daisy_env.delete_old_environment()
+        if self.cleanup_only:
+            return
+        self.daisy_env.create_daisy_server()
+        server = DaisyServer(self.daisy_server_info['name'],
+                             self.daisy_server_info['address'],
+                             self.daisy_server_info['password'],
+                             self.remote_dir,
+                             self.bin_file,
+                             self.adapter)
+        server.connect()
+        server.install_daisy()
+        if self.daisy_only:
+            return
+
+        server.prepare_cluster(self.deploy_file, self.net_file)
+        self.daisy_env.create_or_find_nodes()
+        server.prepare_host_and_pxe()
+        if self.adapter == 'libvirt':
+            LI('Begin Daisy virtual-deploy os and openstack')
+            self.daisy_env.reboot_virtual_nodes()
+            LI('Sleep 20s to wait the VM(s) startup')
+            time.sleep(20)
+            server.install_virtual_nodes()
+
+        LI('The hosts number is %d' % len(self.deploy_struct['hosts']))
+        server.check_os_installation(len(self.deploy_struct['hosts']))
+        time.sleep(10)
+        if self.adapter == 'libvirt':
+            self.daisy_env.reboot_virtual_nodes(boot_devs=['hd'])
+        server.check_openstack_installation(len(self.deploy_struct['hosts']))
+        server.post_deploy()
+        log_bar('Daisy deploy successfully !')
+
+
+def config_arg_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-lab', dest='lab_name', action='store', nargs='?',
+                        default=None,
+                        help='Lab Name')
+    parser.add_argument('-pod', dest='pod_name', action='store', nargs='?',
+                        default=None,
+                        help='Pod Name')
+
+    parser.add_argument('-bin', dest='bin_file', action='store', nargs='?',
+                        default=os.path.join(WORKSPACE, 'opnfv.bin'),
+                        help='OPNFV Daisy BIN File')
+
+    parser.add_argument('-do', dest='daisy_only', action='store_true',
+                        default=False,
+                        help='Install Daisy Server only')
+    parser.add_argument('-co', dest='cleanup_only', action='store_true',
+                        default=False,
+                        help='Cleanup VMs and Virtual Networks')
+    # parser.add_argument('-nd', dest='no_daisy', action='store_true',
+    #                     default=False,
+    #                     help='Do not install Daisy Server when it exists')
+
+    parser.add_argument('-rdir', dest='remote_dir', action='store', nargs='?',
+                        default='/home/daisy',
+                        help='Code directory on Daisy Server')
+
+    parser.add_argument('-wdir', dest='work_dir', action='store', nargs='?',
+                        default='/tmp/workdir',
+                        help='Temporary working directory')
+    parser.add_argument('-sdir', dest='storage_dir', action='store', nargs='?',
+                        default='/home/qemu/vms',
+                        help='Storage directory for VM images')
+    parser.add_argument('-B', dest='pxe_bridge', action='store', nargs='?',
+                        default='pxebr',
+                        help='Linux Bridge for booting up the Daisy Server VM '
+                             '[default: pxebr]')
+    parser.add_argument('-log', dest='deploy_log', action='store', nargs='?',
+                        default=join(WORKSPACE, 'deploy.log'),
+                        help='Path and name of the deployment log file')
+    return parser
+
+
+def parse_arguments():
+    parser = config_arg_parser()
+    args = parser.parse_args()
+
+    save_log_to_file(args.deploy_log)
+    LI(args)
+
+    conf_base_dir = join(WORKSPACE, 'labs', args.lab_name, args.pod_name)
+    deploy_file = join(conf_base_dir, 'daisy/config/deploy.yml')
+    net_file = join(conf_base_dir, 'daisy/config/network.yml')
+
+    check_file_exists(deploy_file)
+    if not args.cleanup_only:
+        check_file_exists(net_file)
+    make_file_executable(args.bin_file)
+
+    confirm_dir_exists(args.work_dir)
+    confirm_dir_exists(args.storage_dir)
+
+    kwargs = {
+        'lab_name': args.lab_name,
+        'pod_name': args.pod_name,
+        'deploy_file': deploy_file,
+        'net_file': net_file,
+        'bin_file': args.bin_file,
+        'daisy_only': args.daisy_only,
+        'cleanup_only': args.cleanup_only,
+        'remote_dir': args.remote_dir,
+        'work_dir': args.work_dir,
+        'storage_dir': args.storage_dir,
+        'pxe_bridge': args.pxe_bridge,
+        'deploy_log': args.deploy_log
+    }
+    return kwargs
+
+
+def main():
+    check_sudo_privilege()
+    kwargs = parse_arguments()
+    deploy = DaisyDeployment(**kwargs)
+    deploy.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ deploy/libvirt_utils.py
@@ -0,0 +1,211 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import commands
+import libvirt
+import os
+import xml.etree.ElementTree as ET
+
+from utils import (
+    LI,
+    LE,
+    LW,
+    WORKSPACE,
+    err_exit,
+    join
+)
+
+
+def get_nets_name(root):
+    nets = []
+    for interface in root.findall('./devices/interface'):
+        if 'type' in interface.attrib and interface.attrib['type'] == 'network':
+            for source in interface.iterfind('source'):
+                if 'network' in source.attrib:
+                    nets.append(source.attrib['network'])
+    return nets
+
+
+def modify_vm_boot_order(root, boot_devs):
+    os_elem = root.find('os')
+    for boot_elem in os_elem.findall('boot'):
+        os_elem.remove(boot_elem)
+    for boot_dev in boot_devs:
+        boot_elem = ET.Element('boot', attrib={'dev': boot_dev})
+        os_elem.append(boot_elem)
+    return root
+
+
+def modify_vm_name(root, vm_name):
+    name_elem = root.find('./name')
+    name_elem.text = vm_name
+
+
+def modify_vm_disk_file(root, disk_file):
+    for disk in root.findall('./devices/disk'):
+        if 'device' in disk.attrib and disk.attrib['device'] == 'disk':
+            for source in disk.iterfind('source'):
+                if 'file' in source.attrib:
+                    source.attrib['file'] = disk_file
+                    break
+
+
+def create_virtual_disk(disk_file, size):
+    LI('Create virtual disk file %s size %d GB' % (disk_file, size))
+    cmd = 'qemu-img create -f qcow2 {disk_file} {size}G'.format(
+        disk_file=disk_file, size=size)
+    status, output = commands.getstatusoutput(cmd)
+    if status:
+        LE(output)
+        err_exit('Fail to create qemu image !')
+
+
+def create_vm(template, name=None, disk_file=None):
+    LI('Begin to create VM %s' % template)
+
+    if name or disk_file:
+        tree = ET.ElementTree(file=template)
+        root = tree.getroot()
+        if name:
+            modify_vm_name(root, name)
+        if disk_file:
+            modify_vm_disk_file(root, disk_file)
+
+        temp_file = join(WORKSPACE, 'tmp.xml')
+        tree.write(temp_file)
+        output = commands.getoutput('cat %s' % temp_file)
+        os.remove(temp_file)
+    else:
+        output = commands.getoutput('cat %s' % template)
+
+    conn = libvirt.open('qemu:///system')
+    domain = conn.defineXML(output)
+    if domain is None:
+        err_exit('Failed to define VM %s' % template)
+    if domain.create() < 0:
+        err_exit('Failed to start VM %s' % template)
+    domain.setAutostart(1)
+
+    LI('VM %s is started' % domain.name())
+    return
+
+
+def reboot_vm(vm_name, boot_devs=None):
+    LI('Begin to reboot VM %s', vm_name)
+    conn = libvirt.open('qemu:///system')
+    try:
+        vm = conn.lookupByName(vm_name)
+    except libvirt.libvirtError, e:
+        LE(e)
+        err_exit('VM %s is not found: ' % vm_name)
+
+    if boot_devs:
+        if vm.isActive():
+            vm.destroy()
+            LI('Destroy VM %s' % vm_name)
+
+        # root = ET.fromstring(vm.XMLDesc())
+        temp_file = join(WORKSPACE, 'tmp.xml')
+        commands.getoutput('virsh dumpxml %s > %s' % (vm_name, temp_file))
+        tree = ET.parse(temp_file)
+        root = tree.getroot()
+        LI('Modify the boot order %s' % boot_devs)
+        modify_vm_boot_order(root, boot_devs)
+        tree.write(temp_file)
+
+        LI('Re-define and start the VM %s' % vm_name)
+        vm.undefine()
+        vm = conn.defineXML(commands.getoutput('cat %s' % temp_file))
+        vm.create()
+        vm.setAutostart(1)
+    else:
+        vm.reset()
+
+    conn.close()
+
+
+def get_disk_file(root):
+    disks = []
+    for disk in root.findall('./devices/disk'):
+        if 'device' in disk.attrib and disk.attrib['device'] == 'disk':
+            for source in disk.iterfind('source'):
+                if 'file' in source.attrib:
+                    disks.append(source.attrib['file'])
+    return disks
+
+
+def delete_vm_and_disk(vm_name):
+    LI('Begin to delete VM %s', vm_name)
+    conn = libvirt.open('qemu:///system')
+    vm = None
+    for item in conn.listAllDomains():
+        if vm_name == item.name():
+            vm = item
+            break
+    if vm is None:
+        conn.close()
+        LI('VM %s is not found' % vm_name)
+        return
+
+    output = vm.XMLDesc()
+    root = ET.fromstring(output)
+
+    if vm.isActive():
+        vm.destroy()
+        LI('Destroy VM %s' % vm.name())
+    vm.undefine()
+
+    for disk_file in get_disk_file(root):
+        if os.path.isfile(disk_file):
+            status, output = commands.getstatusoutput('rm -f %s' % disk_file)
+            if status:
+                LW('Failed to delete the VM disk file %s' % disk_file)
+
+    conn.close()
+    LI('VM %s is removed' % vm_name)
+
+
+def create_virtual_network(template):
+    LI('Begin to create virtual network %s' % template)
+    output = commands.getoutput('cat %s' % template)
+    conn = libvirt.open('qemu:///system')
+    network = conn.networkDefineXML(output)
+    if network is None:
+        err_exit('Failed to define a virtual network %s' % template)
+
+    network.create()  # set the network active
+    network.setAutostart(1)
+    conn.close()
+    LI('Virtual network %s is created' % network.name())
+    return network.name()
+
+
+def delete_virtual_network(network_xml):
+    LI('Begin to find and delete network %s' % network_xml)
+    tree = ET.ElementTree(file=network_xml)
+    root = tree.getroot()
+    names = root.findall('./name')
+    assert len(names) == 1
+    name = names[0].text
+
+    result = 0
+    conn = libvirt.open('qemu:///system')
+
+    for net in conn.listAllNetworks():
+        if name == net.name():
+            if net.isActive():
+                net.destroy()
+                LI('Network %s is destroyed' % name)
+            net.undefine()
+            LI('Network %s is deleted' % name)
+            result = 1
+            break
+    conn.close()
+    if not result:
+        LI('Network %s is not found' % name)
