6e3b2573b5d856a80aaf12382dfa83b053f85adb
Alex Yang <yangyang1@zte.com.cn>
2017-05-25 15:02:43 +0800

    Remove the incomplete file and re-download it
    The kolla file downloaded on the ci node is incomplete.
    
    Change-Id: Id15695504bd29f31178054b5e8cc83d8612543d9
    Signed-off-by: Alex Yang <yangyang1@zte.com.cn>

--- code/makefile_patch.sh
+++ code/makefile_patch.sh
@@ -25,19 +25,35 @@ cirros_ver="0.3.5"
 cirros_filename="cirros-${cirros_ver}-x86_64-disk.img"
 cirros_url=${cirros_server}/${cirros_ver}/${cirros_filename}
 
+function check_or_download_file()
+{
+    file_path=$1
+    file_url=$2
+    file_name=$(basename $2)
+
+    if [ ! -f ${file_path}/${file_name} ]; then
+        wget -P ${file_path} --progress=dot:giga ${file_url}
+    else
+        wget --spider $file_url -o tmp_filesize
+        origin_size=$(cat tmp_filesize | grep Length | awk '{print $2}')
+        rm tmp_filesize
+        local_size=$(stat -c %s ${file_path}/${file_name} | tr -d '\n')
+
+        if [ $local_size -ne $origin_size ]; then
+            echo "The local file is incomplete. Re-download it."
+            rm -f $file_path/$file_name
+            wget -P ${file_path} --progress=dot:giga ${file_url}
+        else
+            echo "File $file_path/$file_name is ok."
+        fi
+    fi
+}
+
 if [ ! -d $CACHE_PATH ]; then mkdir -p $CACHE_PATH ; fi
-if [ ! -f $CACHE_PATH/${isoname} ]; then
-    wget -P $CACHE_PATH $isourl
-fi
-if [ ! -f $CACHE_PATH/$imagename ]; then
-    wget -P $CACHE_PATH "$imageserver/$imagename"
-fi
-if [ ! -f $CACHE_PATH/registry-server.tar ]; then
-    wget -P $CACHE_PATH "http://daisycloud.org/static/files/registry-server.tar"
-fi
-if [ ! -f $CACHE_PATH/${cirros_filename} ]; then
-    wget -P $CACHE_PATH ${cirros_url}
-fi
+check_or_download_file $CACHE_PATH $isourl
+check_or_download_file $CACHE_PATH $imageserver/$imagename
+check_or_download_file $CACHE_PATH "http://daisycloud.org/static/files/registry-server.tar"
+check_or_download_file $CACHE_PATH ${cirros_url}
 
 cp $CACHE_PATH/${isoname} $TOOLS_PATH/setup/bin_temp/
 cp $CACHE_PATH/$imagename $TOOLS_PATH/setup/bin_temp/
