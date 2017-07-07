69a1af39f02bb5c6a471768648c15f8ae1e85776
zhongjun <zhong.jun@zte.com.cn>
2017-07-07 09:07:48 +0800

    Enable HA scenarios
    Enable HA scenario options due to support HA function in upsteam.
    
    Change-Id: Ie1889afc1a149f171a9b324eb284fd650baed397
    Signed-off-by: zhongjun <zhong.jun@zte.com.cn>

--- ci/deploy/deploy.sh
+++ ci/deploy/deploy.sh
@@ -63,7 +63,9 @@ TARGET_HOSTS_NUM=0
 DRY_RUN=0
 IS_BARE=1
 VM_MULTINODE=("computer01" "computer02" "controller02" "controller03" "controller01")
-VALID_DEPLOY_SCENARIO=("os-nosdn-nofeature-noha" "os-nosdn-nofeature-ha" "os-odl_l3-nofeature-noha" "os-odl_l2-nofeature-noha")
+VALID_DEPLOY_SCENARIO=("os-nosdn-nofeature-noha" "os-nosdn-nofeature-ha" "os-odl_l3-nofeature-noha"
+                       "os-odl_l2-nofeature-noha" "os-odl_l3-nofeature-ha" "os-odl_l2-nofeature-ha")
+
 #
 # END of variables to customize
 ############################################################################
