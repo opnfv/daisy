286f16b718b2adea14e9a4619850c47ef7b99f22
Alex Yang <yangyang1@zte.com.cn>
2017-08-04 17:23:23 +0800

    Ignore oder in unit test of test_get_conf.py
    Since the result contains a list, so ignore_order should be True
    in the comparison.
    
    Change-Id: Iff4c655312015a55a69b91692ab6f9696715a143
    Signed-off-by: Alex Yang <yangyang1@zte.com.cn>

--- tests/unit/test_get_conf.py
+++ tests/unit/test_get_conf.py
@@ -9,7 +9,7 @@
 
 import os
 import pytest
-
+import pprint
 from deepdiff import DeepDiff
 
 from deploy.get_conf import (
@@ -64,4 +64,4 @@ def test_config(deploy_file, network_file):
               '10.20.11.11', '/dev/sdb',
               {'controller01': [], 'controller02': [], 'controller03': [],
                'computer01': [], 'computer02': []})
-    assert DeepDiff(result, expect) == {}
+    assert DeepDiff(result, expect, ignore_order=True) == {}
