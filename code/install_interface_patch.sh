944dbc42916fb8c8590aaa803dba1ee8422fdeb3
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2017-11-22 04:43:32 -0500

    Switch ODL version to Carbon-SR1
    SR2 seems has issues with FIP ping test
    
    Change-Id: I4aea097f2009c2c33de8fc85820bf68c1cbb316a
    Signed-off-by: Zhijiang Hu <hu.zhijiang@zte.com.cn>

--- code/install_interface_patch.sh
+++ code/install_interface_patch.sh
@@ -9,7 +9,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 imagebranch="pike"
-imageversion="171117113539"
+imageversion="171121172754"
 imageserver="http://artifacts.opnfv.org/daisy/upstream"
 imagedir="/var/lib/daisy/versionfile/kolla"
 imagename="kolla-image-$imagebranch-$imageversion.tgz"
