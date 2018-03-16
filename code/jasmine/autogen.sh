9fb1b507dc7a77db2f9df7541932b43e88a3a4b3
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2018-03-16 13:33:51 +0800

    Fix Licence header missing issues
    Change-Id: I186f94b6e7580974a32a3c3c48fdc891b8445937
    Signed-off-by: Zhijiang Hu <hu.zhijiang@zte.com.cn>

--- code/jasmine/autogen.sh
+++ code/jasmine/autogen.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+##############################################################################
+# Copyright (c) 2018 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
 # Run this to generate all the initial makefiles, etc.
 mkdir -p m4
 echo Building configuration system...
