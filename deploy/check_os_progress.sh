5a06efe1052bcb6117599017dd4b34bfca8e0fd5
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2018-03-16 13:33:51 +0800

    Fix Licence header missing issues
    Change-Id: I186f94b6e7580974a32a3c3c48fdc891b8445937
    Signed-off-by: Zhijiang Hu <hu.zhijiang@zte.com.cn>

--- deploy/check_os_progress.sh
+++ deploy/check_os_progress.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+##############################################################################
+# Copyright (c) 2018 ZTE Coreporation and others.
+# hu.zhijiang@zte.com.cn
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
 usage()
 {
     cat << EOF
